4a2181cdcd443521f46f1d522d03b751b56bb234
ziclude <cindyzickziemi@gmail.com>
2022-03-17 04:04:41 -0700

    print numbers

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,1 +1,15 @@
 #include "main.h"
+
+/**
+ * _isdigit - checks for digit
+ * @c: character to checked for digit membership
+ *
+ * Return: 1 if it is digit, 0 if not
+ */
+
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+return (1);
+return (0);
+}
--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,1 +1,17 @@
 #include "main.h"
+
+/**
+ * print_numbers - prints numbers between 0 and 9
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+int c;
+for (c = '0'; c <= '9'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
