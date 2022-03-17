759750d868eaf1c9a2a0b235cf4af4584be86120
ziclude <cindyzickziemi@gmail.com>
2022-03-17 08:08:24 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,18 +14,18 @@ int num;
 for (num = 1; num <= 100; num++)
 {
 if ((num % 3) == 0 && (num % 5) == 0)
-printf("FizzBuzz");
+_putchar("FizzBuzz");
 else if ((num % 3) == 0)
-printf("Fizz");
+_putchar("Fizz");
 else if ((num % 5) == 0)
-printf("Buzz");
+_putchar("Buzz");
 else
-printf("%d", num);
+_putchar("%d", num);
 if (num == 100)
 continue;
-printf(" ");
+_putchar(" ");
 }
-printf("\n");
+_putchar("\n");
 
 return (0);
 }
