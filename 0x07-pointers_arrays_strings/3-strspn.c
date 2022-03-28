184463c28babf788a4ab3d5b3fd93daa417beac4
ziclude <cindyzickziemi@gmail.com>
2022-03-28 05:21:29 -0700

    updated

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,27 +1,31 @@
 #include "main.h"
-#define NULL 0
 
 /**
- * _strchr - locates a character in a string
- * @s: string
- * @c: character to search
- * Return: pointer to first occurrence of c
+ * _strspn - Gets the length of a prefix substring.
+ * @s: The string to be searched.
+ * @accept: The prefix to be measured.
+ *
+ * Return: The number of bytes in s which
+ * consist only of bytes from accept.
  */
 
-char *_strchr(char *s, char c)
+unsigned int _strspn(char *s, char *accept)
 {
-int i = 0;
-while (*(s + i) != '\0')
+unsigned int bytes = 0;
+int index;
+while (*s)
 {
-if (*(s + i) == c)
+for (index = 0; accept[index]; index++)
 {
-return (s + i);
+if (*s == accept[index])
+{
+bytes++;
+break;
 }
-i++;
+else if (accept[index + 1] == '\0')
+return (bytes);
 }
-if (c == '\0')
-{
-return (s + i);
+s++;
 }
-return (NULL);
+return (bytes);
 }
