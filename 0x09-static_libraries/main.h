14c2da34845aebbb0fe4269dee8fd8708325fe54
ziclude <cindyzickziemi@gmail.com>
2022-04-01 10:39:25 -0700

    main update

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,4 +1,5 @@
-
+#ifndef MAIN_H
+#define MAIN_H
 /**
 * File: main.h
 * 
@@ -26,3 +27,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
+#endif
