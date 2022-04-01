af19e88c8b6b09868a6c4ba3f77ed29517dde095
ziclude <cindyzickziemi@gmail.com>
2022-04-01 11:49:41 -0700

    update

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,15 +8,9 @@
  * Return: 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc, __attribute__((unused)) char *argv[])
 {
-int i;
-if (argc == 1)
-printf("%d\n", argc - 1);
-else
-{
-for (i = 0; *argv; i++, argv++)
-printf("%d\n", i - 1);
-}
+int args = argc - 1;
+printf("%d\n", args);
 return (0);
 }
