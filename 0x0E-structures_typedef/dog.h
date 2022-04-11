ccfe7bc3159f536ab05ed789a2713d024b3d31db
ziclude <cindyzickziemi@gmail.com>
2022-04-11 04:12:28 -0700

    redog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,18 +13,18 @@
  * @owner: The owner of the dog.
  */
 
-struct dog
+typedef struct dog
 {
 char *name;
 float age;
 char *owner;
-};
+} dog_t;
 
-/**
+/*
  * dog_t - Typedef for struct dog
- */
+ *
 
-typedef struct dog dog_t;
+ typedef struct dog dog_t;*/
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
