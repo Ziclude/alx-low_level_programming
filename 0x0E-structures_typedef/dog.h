81b13a69d20a3cedea4341101ea43e53eae4a6e7
ziclude <cindyzickziemi@gmail.com>
2022-04-11 04:15:03 -0700

    poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -22,9 +22,8 @@ char *owner;
 
 /*
  * dog_t - Typedef for struct dog
- *
-
- typedef struct dog dog_t;*/
+ *typedef struct dog dog_t;
+*/
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
