23e9dbffe9bed14b19ace5f910529233d1af6282
ziclude <cindyzickziemi@gmail.com>
2022-06-09 06:10:03 -0700

    Re:zero

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,12 +9,12 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-size_t nods = 0;
+size_t nodes = 0;
 while (h)
 {
-nods++;
+nodes++;
 printf("%d\n", h->n);
 h = h->next;
 }
-return (nods);
+return (nodes);
 }
